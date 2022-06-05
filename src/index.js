14d88c934de286722b7e486c4ef44ebecbd4d0c1
Max <vghuhsd@gmail.com>
2022-06-05 22:28:31 +0300

    Fixed issues @varadero

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ export default grapesjs.plugins.add('gjs-plugin-ckeditor5', (editor, opts = {}) 
   };
   
   let c = {...defaults, ...opts};
-  console.log(c);
+
   if (!ClassicEditor) {
     throw new Error('CKEDITOR instance not found');
   }
@@ -31,8 +31,21 @@ export default grapesjs.plugins.add('gjs-plugin-ckeditor5', (editor, opts = {}) 
         ClassicEditor.create(document.querySelector(`#${el.id}ckeditor`),c.options);
       });
       modal.onceClose(() => {
-        if(cashDom(`#${el.id}ckeditor`).hasClass('save')) {
-          cashDom(`#${el.id}ckeditor`).html(cashDom(`#mckeditor${el.id} .ck-editor .ck-content`).html());
+        const ckblock = document.querySelector(`.ck-body-wrapper`);
+        ckblock.innerHTML = '';
+      });
+      modal.open({
+        title: 'Edit',
+        content: `<style>.modalckeditor .ck-editor .ck-editor__editable_inline {min-height:400px}.modalckeditor .ck-editor .ck-content * {color: black;}.modalckeditor .gjs-btn-prim{
+          margin: 10px 5px 5px 0;
+      }</style><div id="${el.id}ckeditor">${el.innerHTML}</div><input type="button" class="gjs-btn-prim" id="BtnSave" onClick="document.getElementById('${el.id}ckeditor').classList.add('save');" value="Save" title="save"><input class="gjs-btn-prim" id="BtnClose" type="button" value="Close" title="close">`,
+        attributes: {
+          class: `modalckeditor `,
+          id: `mckeditor${el.id}`
+        }
+      });
+      document.getElementById('BtnSave').onclick = function(){
+        cashDom(`#${el.id}ckeditor`).html(cashDom(`#mckeditor${el.id} .ck-editor .ck-content`).html());
           cashDom(`#${el.id}ckeditor figure.table.ck-widget`).map((index, element) => {
             cashDom(element).find('table').insertAfter(element);
             cashDom(element).remove();
@@ -44,21 +57,19 @@ export default grapesjs.plugins.add('gjs-plugin-ckeditor5', (editor, opts = {}) 
           cashDom(`.ck-fake-selection-container`).map((index, element) => {
             cashDom(element).remove();
           })
-          editor.getSelected().components('');
-          editor.getSelected().set('content', cashDom(`#${el.id}ckeditor`).html());
+          
+          if(editor.getSelected().getEl().innerHTML != cashDom(`#${el.id}ckeditor`).html()){
+            editor.getSelected().components('');
+            editor.getSelected().set('content', cashDom(`#${el.id}ckeditor`).html());
+          }
+        
           cashDom(`#${el.id}ckeditor`).removeClass('save');
-        }
-      });
-      modal.open({
-        title: 'Edit',
-        content: `<style>.modalckeditor .ck-editor .ck-editor__editable_inline {min-height:400px}.modalckeditor .ck-editor .ck-content * {color: black;}.modalckeditor .gjs-btn-prim{
-          margin: 10px 5px 5px 0;
-      }</style><div id="${el.id}ckeditor">${el.innerHTML}</div><input type="button" class="gjs-btn-prim" onClick="document.getElementById('${el.id}ckeditor').classList.add('save');editor.Modal.close()" value="Save" title="save"><input class="gjs-btn-prim" type="button" onClick="editor.Modal.close()" value="Close" title="close">`,
-        attributes: {
-          class: `modalckeditor `,
-          id: `mckeditor${el.id}`
-        }
-      });
+
+        modal.close();
+      }
+      document.getElementById('BtnClose').onclick = function(){
+        modal.close();
+      }
     },
     focus(el, rte) {
       el.contentEditable = true;
